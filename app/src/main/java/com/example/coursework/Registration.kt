ac652a5f5b46a8916d532a483686ecca3fc78e6e
Alexandr <alex@mail.ru>
2025-12-22 14:47:42 +0300

    настроена страница регистрации

--- app/src/main/java/com/example/coursework/Registration.kt
+++ app/src/main/java/com/example/coursework/Registration.kt
@@ -1,8 +1,8 @@
 package com.example.coursework
 
 import android.content.Intent
-import android.graphics.Paint
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -25,35 +25,44 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.OffsetMapping
-import androidx.compose.ui.text.input.TransformedText
-import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.coursework.ui.theme.CourseWorkTheme
 
-class PreLOGIN : ComponentActivity() {
+class Registration : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            //val phone = remember{mutableStateOf("")}
             val context = LocalContext.current // Получаем текущий Context
-            val intent1 = Intent(context, MainActivity::class.java)
-            val intent2 = Intent(context, Registration::class.java)
+            val intent1 = Intent(context, PreLOGIN::class.java)
+            //val intent2 = Intent(context, Registration::class.java)
             //var text  by remember { mutableStateOf("") }
             val message1 = remember{mutableStateOf("")}
             val message2 = remember{mutableStateOf("")}
+            val message3 = remember{mutableStateOf("")}
+            val message4 = remember{mutableStateOf("")}
 
             CourseWorkTheme {
-
                 Box(modifier = Modifier.fillMaxSize()) {
                     Column (modifier = Modifier.align(Alignment.TopCenter)){
+                        Spacer(modifier = Modifier.height(30.dp))
+                        Text("Имя")
+                        //Text(message.value, fontSize = 28.sp)
+                        TextField(value = message3.value, onValueChange = {newText -> message3.value = newText},
+                            textStyle = TextStyle(fontSize = 28.sp),
+
+                        )
+                        Spacer(modifier = Modifier.height(30.dp))
+                        Text("Фамилия")
+                        //Text(message.value, fontSize = 28.sp)
+                        TextField(value = message4.value, onValueChange = {newText -> message4.value = newText},
+                            textStyle = TextStyle(fontSize = 28.sp),
+
+                        )
                         Spacer(modifier = Modifier.height(30.dp))
                         Text("Номер телефона")
                         //Text(message.value, fontSize = 28.sp)
@@ -64,7 +73,7 @@ class PreLOGIN : ComponentActivity() {
                         Spacer(modifier = Modifier.height(16.dp))
                         Text("Пароль")
                         TextField(value = message2.value, onValueChange = {newText -> message2.value = newText},
-                            visualTransformation = { filter(it) }
+                            //visualTransformation = { filter(it) }
                         )
                     }
                     Row (modifier = Modifier.align(Alignment.Center)){
@@ -77,25 +86,15 @@ class PreLOGIN : ComponentActivity() {
                         Spacer(modifier = Modifier.width(16.dp))
                         Button(
                             onClick = {
-                                context.startActivity(intent2)
-                            },
+                                Toast.makeText(context, "Данные сохранены", Toast.LENGTH_SHORT).show()
+                                //context.startActivity(intent2)
+                            }
                         )
                         { Text("Зарегистрироваться")}
 
-                    }
-                }
-            }
         }
     }
 }
 
-fun filter(text: AnnotatedString): TransformedText{
-    return TransformedText(
-        AnnotatedString("*".repeat(text.text.length)),
-        /**
-         * [OffsetMapping.Identity] is a predefined [OffsetMapping] that can be used for the
-         * transformation that does not change the character count.
-         */
-        OffsetMapping.Identity,
-    )
-}
+
+}}}
